b7c93f30e1cedcb18b3da29f7dd4890b01c125d7
Arab45 <horlakunleolayiwola@gmail.com>
2026-08-03 14:51:14 -0700

    signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./pages/header"
 import Login from "./pages/login"
+import Signup from "./pages/signup"
 
 function App() {
   return (
     <div className="bg-slate-50 min-h-screen">
       {/* <Header /> */}
-      <Login />
+      {/* <Login /> */}
+      <Signup />
     </div>
   )
 }
