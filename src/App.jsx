971e1451be66b111d5f62487e14efa682206b115
Arab45 <horlakunleolayiwola@gmail.com>
2026-08-06 15:06:55 -0700

    onboarding

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import Header from "./pages/header"
-import Login from "./pages/login"
-import Signup from "./pages/signup"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./pages/header";
+import Login from "./pages/login";
+import Signup from "./pages/signup";
+import Onboarding from "./pages/onboarding";
 
 function App() {
   return (
-    <div className="bg-slate-50 min-h-screen">
-      {/* <Header /> */}
-      {/* <Login /> */}
-      <Signup />
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Onboarding />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/header" element={<Header />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -3,21 +3,49 @@ import logo from "../assets/logo/EDU.png";
 function Signup() {
   return (
     <div className="flex min-h-screen bg-background text-primary">
-      <div className="flex items-center justify-center w-1/2 h-screen">
+      <div className="flex flex-col justify-between w-1/2 h-screen p-4">
         {/* <img src={logo} alt="logo" className='sm:w-screen h-32'/> */}
+        <div className="">
         <p className="font-bold sm:text-2xl p-2">
           EDUCATION DEVELOPMENT UNIT HUB
         </p>
+        <div className="flex flex-col gap-2 p-2">
+          <p>Connect with brilliant minds. Collarate on groundbreaking research. Shape the future of discovery.</p>
+        </div>
+        </div>
+
+        <div>
+            <div className="flex gap-4 mb-2">
+                <div className="flex items-center justify-center h-6 w-6 bg-slate-400 rounded-full border-0">1</div>
+                <p>Account Type</p>
+            </div>
+            <div className="flex gap-4 mb-2">
+                <div className="flex items-center justify-center h-6 w-6 bg-orange-400  rounded-full border-0">2</div>
+                <p>Basic Info</p>
+            </div>
+            <div className="flex gap-4 mb-2">
+                <div className="flex items-center justify-center h-6 w-6 bg-slate-400 rounded-full border-0">3</div>
+                <p>Profile Details</p>
+            </div>
+
+            <div className="flex gap-4 mb-2">
+                <div className="flex items-center justify-center h-6 w-6 bg-slate-400 rounded-full border-0">4</div>
+                <p>Interests</p>
+            </div>
+        </div>
       </div>
-      <div className="flex w-1/2 bg-slate-50 h-screen text-black items-center justify-center">
-        <div className="gap-4">
-          <p className="font-bold text-xl text-slate-900 text-left mx-4 mb-4">
-            Sign Up
+      <div className="flex w-1/2 bg-slate-50 h-screen text-black">
+        <div className="flex flex-col gap-4 w-full items-center justify-center">
+          <div className="flex flex-col">
+          <p className="font-bold text-xl">
+            Basic Information
           </p>
+          <p className="">let's start with the essential</p>
+          </div>
           <div className="">
             <div>
               <label className="mx-4 font-semibold text-slate-900">
-                Full Name
+                Full Name *
               </label>
               <br />
               <input
@@ -27,7 +55,7 @@ function Signup() {
             </div>
 
             <div>
-              <label className="mx-4 font-semibold text-slate-900">Email</label>
+              <label className="mx-4 font-semibold text-slate-900">Email Address *</label>
               <br />
               <input
                 className="border rounded p-1.5 mb-2 w-2xs mx-4"
@@ -37,7 +65,7 @@ function Signup() {
 
             <div>
               <label className="mx-4 font-semibold text-slate-900">
-                Phone Number
+                Phone Number *
               </label>
               <br />
               <input
@@ -47,12 +75,12 @@ function Signup() {
             </div>
             <div>
               <label className="mx-4 font-semibold text-slate-900">
-                Password
+                Password *  
               </label>
               <br />
               <input
                 className="border rounded p-1.5 w-2xs mx-4"
-                placeholder="******"
+                placeholder="Create a strong password"
               />
             </div>
             <button className="bg-background text-primary p-2 mx-4 mt-2 w-2xs border rounded font-semibold">
